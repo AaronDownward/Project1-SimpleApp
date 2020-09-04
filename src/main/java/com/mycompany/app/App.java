7dc994ada017634f94d0db5afdaee381bfc532fb
AaronDownward <downward150@gmail.com>
2020-09-04 10:53:13 -0500

    please work for demo

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,16 @@
 package com.mycompany.app;
 
 import java.time.LocalTime;
+import java.util.Scanner;
 
 public class App 
 {
     public static void main(String[] args) {
         System.out.println("The local time is: " + LocalTime.now().toString());
+        System.out.println("Tell us something nice...");
+        Scanner scanner = new Scanner(System.in);
+        String output = scanner.nextLine();
+        System.out.println(output + " is really nice");
+        
     }
 }
